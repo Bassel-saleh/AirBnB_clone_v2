b7fffd6996000c63be0068ed571e2e75f7406953
Bassel-saleh <basselh26@gmai.com>
2024-04-21 09:37:31 +0200

    added task#1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Starts a Flask web application
 
-The application listens on 0.0.0.0, port 5000.
+The application listens on 0.0.0.0, port 5000
 Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
+    /: Displays 'Hello HBNB!'
+    /hbnb: Displays 'HBNB'
 """
 from flask import Flask
 
